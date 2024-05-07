e0a62c8888ebb465d3f19e0f59a3c5d9188c1351
Munieru <20086673+munierujp@users.noreply.github.com>
2024-05-07 22:03:26 +0900

    Migrate to ESM (#162)
    * Add `"type": "module"`
    
    * Rename `.stylelintrc.js` to `.stylelintrc.cjs`
    
    * Add type

---
+++ .stylelintrc.cjs
@@ -0,0 +1,13 @@
+// @ts-check
+
+/** @typedef {import('stylelint').Config} Config */
+
+/**
+ * @type {Config}
+ * @see https://stylelint.io/user-guide/configure/
+ */
+const config = {
+  extends: 'stylelint-config-standard'
+}
+
+module.exports = config
