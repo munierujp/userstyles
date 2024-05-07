6765a0149dee6a8fdd39fd649dcba7485f70415f
Munieru <20086673+munierujp@users.noreply.github.com>
2024-05-07 22:01:09 +0900

    Add type

--- .stylelintrc.cjs
+++ .stylelintrc.cjs
@@ -1,3 +1,13 @@
-module.exports = {
+// @ts-check
+
+/** @typedef {import('stylelint').Config} Config */
+
+/**
+ * @type {Config}
+ * @see https://stylelint.io/user-guide/configure/
+ */
+const config = {
   extends: 'stylelint-config-standard'
 }
+
+module.exports = config
