86a12b90e4cbc85bfe30126edd77727bf0171e1a
Munieru <20086673+munierujp@users.noreply.github.com>
2024-09-28 10:31:19 +0900

    Update stylelint rules (#172)

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -7,7 +7,11 @@
  * @see https://stylelint.io/user-guide/configure/
  */
 const config = {
-  extends: 'stylelint-config-standard'
+  extends: 'stylelint-config-standard',
+  rules: {
+    'selector-class-pattern': null,
+    'selector-id-pattern': null
+  }
 }
 
 export default config
